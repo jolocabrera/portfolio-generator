95d5d05ba89bbfb449f3d232779580f1c7c26d70
Jolo <jolocabrera@yahoo.com>
2022-05-17 13:44:04 -0700

    html template handles data for projects and about me now

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template");
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
 // const pageHTML = generatePage(name, github)
 
@@ -8,8 +8,6 @@ const inquirer = require("inquirer");
 // fs.writeFile('index.html', pageHTML, err => {
 //     if (err) throw err;
 
-//     console.log("Porfolio complete! Check out index.html to see the output!");
-// });
 
 
 const promptUser = () => {
@@ -144,5 +142,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
